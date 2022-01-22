a8a3180e510e0985f9d36682738706ddcfad4635
LebryantJohnson <69935607+LebryantJohnson@users.noreply.github.com>
2022-01-22 14:46:07 -0800

    added cpp functionality

---
+++ Source/SurvivalInventory/testactorrrider.cpp
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "testactorrrider.h"
+
+
+// Sets default values
+Atestactorrrider::Atestactorrrider()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void Atestactorrrider::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void Atestactorrrider::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
